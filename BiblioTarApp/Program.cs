d5f04133107c1390002bf2780c5dbe89d28f26f2
Márk Papp <papp.mark04@gmail.com>
2026-04-23 07:00:42 +0200

    vmi

--- BiblioTarApp/Program.cs
+++ BiblioTarApp/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo{ Title = "BiblioTarApp API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BiblioTarApp API", Version = "v1" });
 });
 
 var app = builder.Build();
