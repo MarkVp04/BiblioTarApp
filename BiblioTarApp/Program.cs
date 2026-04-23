4cafe1cfae0e29cb8c2a3d791305a9b64100d927
Márk Papp <papp.mark04@gmail.com>
2026-04-23 07:02:49 +0200

    asd

--- BiblioTarApp/Program.cs
+++ BiblioTarApp/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo{ Title = "BiblioTarApp API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo{ Title = "BiblioTarApp API", Version = "v1" }); //teszt
 });
 
 var app = builder.Build();
